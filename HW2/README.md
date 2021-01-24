6a8c8fb520544e888e038c1b9ce703de8793d946
alexdostew <Alexdostew@outlook.com>
2021-01-24 12:45:11 -0700

    add homework 2

---
+++ HW2/README.md
@@ -0,0 +1,5 @@
+# Alexander Stewart
+## Homework 2 Readme
+
+This homework assignment was mostly just review for me.
+One thing I noticed I kept doing on accident was putting quotations around numbers when using console.log
---
+++ HW2/script.js
@@ -0,0 +1,14 @@
+/*
+Alexander Stewart
+Homework 2
+JavaScript logs and comments
+*/
+
+// Favorite websites
+console.log("Favorite websites: Youtube, Twitch, Spotify");
+
+// Favorite games
+console.log("Favorite games: Rocket League, Escape from Tarkov, Dark Souls.");
+
+// Favorite artists
+console.log("Favorite artists: Pendulum, Nirvana, 9mm Parabellum Bullet");
